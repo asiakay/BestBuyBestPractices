4a544b35551e170194a18dec3d52e709369e6218
Asia Grady <66960776+asiakay@users.noreply.github.com>
2020-07-29 18:05:57 -0400

    pushed repository from command line

--- BestBuyBestPractices/IDepartmentRepository.cs
+++ BestBuyBestPractices/IDepartmentRepository.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 namespace BestBuyBestPractices
 {
     public interface IDepartmentRepository
     {
+        IEnumerable<Department> GetAllDepartments();
     }
 }
--- BestBuyBestPractices/IProductRepository.cs
+++ BestBuyBestPractices/IProductRepository.cs
@@ -1,7 +1,14 @@
 ﻿using System;
+using System.Collections.Generic;
+
 namespace BestBuyBestPractices
 {
     public interface IProductRepository
     {
+        IEnumerable<Product> GetAllProducts();
     }
+    public void CreateProduct(string name, int price, int categoryID)
+    {
+
+    } 
 }
